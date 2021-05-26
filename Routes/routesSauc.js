9d6989c5924c6df9d6057332186998fc82555964
matthieu661 <matthieu661@outlook.fr>
2021-05-26 17:17:31 +0200

    middleware getAllSauces fonctionel (basique) : no erreur ! :----------> getOneSauce fonctionel

--- Routes/routesSauc.js
+++ Routes/routesSauc.js
@@ -11,21 +11,17 @@ const Multer =require('../Middleware/Multer-config');
 // MIDDLEWARES  (Projet 6 demande 8 middlewares) : 4- "Post" ; 1- "Put" ; 1- "Delete" ; 2 "Get"  ( 2 dans routesAuth.js login, signup)
 
 
-routerSauces.post('/', Authentification, Multer, CtrlSauces.createSauces);
+routerSauces.post('/', Authentification, Multer, CtrlSauces.createSauces); // create
 
-/*
-routerSauces.post((req, res, next) => {
-    res.json({ message: " express ok"});
-});
+//routerSauces.post('/:id/like', Authentification, CtrlSauces.likeSauce); // like
 
-            Likes
-  
-*/
+routerSauces.put('/:id', Authentification, Multer, CtrlSauces.modifySauces); // modify
 
-routerSauces.put('/:id', Authentification, Multer, CtrlSauces.modifySauces);
-routerSauces.delete('/:id',Authentification, CtrlSauces.deleteSauces);
-routerSauces.get('/:id',Authentification, CtrlSauces.getOneSauces);
-routerSauces.get('/sauces',Authentification, CtrlSauces.getAllSauces);
+routerSauces.delete('/:id', Authentification, CtrlSauces.deleteSauces); // delete
+
+routerSauces.get('/:id', Authentification, CtrlSauces.getOneSauces); // get one
+
+routerSauces.get('/', Authentification, CtrlSauces.getAllSauces); // get all
 
 
 module.exports = routerSauces;
